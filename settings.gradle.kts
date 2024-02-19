6cdeaac33601dde9da9a5fd53dfe613efb3fb90a
Jayanth Sai Varma Uppalapati <126601899+Jay-4331@users.noreply.github.com>
2024-02-18 21:06:04 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Login_And_ Register"
+include(":app")
+ 
